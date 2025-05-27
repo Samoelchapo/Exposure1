348a15265c541663b5522ba928910c985b73ba32
20556 <samuel.meneses@vtsd.com>
2025-05-27 14:55:09 -0300

    1st day

---
+++ introduction/VariableIntro.java
@@ -0,0 +1,13 @@
+package introduction;
+
+public class VariableIntro {
+    public static void main(String[] args) {
+
+        double salary = 10000.56;
+        int bonus = 250;
+
+        bonus=(int) salary;
+        System.out.println(salary);
+        System.out.println(bonus);
+    }
+}
